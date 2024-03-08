df4aeb676efe5c8cb9e28c9399c9088dec9b9943
RodCri <cristian.rodriguez1317@gmail.com>
2024-03-08 18:32:14 -0500

    Change BD MySQL

--- api/libs/sequelize.js
+++ api/libs/sequelize.js
@@ -5,9 +5,9 @@ const setupModels = require('./../db/models');
 
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI=`postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const URI=`mysql://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
-const sequelize = new Sequelize(URI,{dialect:'postgres',logging:true,});
+const sequelize = new Sequelize(URI,{dialect:'mysql',logging:true,});
 
 setupModels(sequelize);
 
