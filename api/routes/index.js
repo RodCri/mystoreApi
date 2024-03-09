5e15feafcc7ca4b8fc2f0dc58fa6ee695f37dbc0
RodCri <cristian.rodriguez1317@gmail.com>
2024-03-09 06:41:21 -0500

    Add belongsTo relation

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,16 +2,17 @@ const express = require('express');
 const productsRouter = require('./products.router');
 const usersRouter = require('./users.router');
 const categoriesRouter = require('./categories.router');
+const customersRouter = require('./customers.router');
 
 
 function routerApi(app){
   // Establecer un path global
   const router = express.Router();
   app.use('/api/v1', router)
-
   router.use('/products', productsRouter);
   router.use('/users', usersRouter);
   router.use('/categories', categoriesRouter);
+  router.use('/customers', customersRouter);
 }
 
 module.exports = routerApi;
